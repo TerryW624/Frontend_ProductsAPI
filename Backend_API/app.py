abcc0c97bc06c58790ba023c8cea79207c6c2466
TerryW624 <terrywade634@gmail.com>
2023-06-22 12:13:29 -0700

    Backend Resources, Schemas, product class & routes

--- Backend_API/app.py
+++ Backend_API/app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from marshmallow import post_load, fields, ValidationError
 from dotenv import load_dotenv
 from os import environ
 
@@ -23,15 +24,73 @@ CORS(app)
 Migrate(app, db)
 
 # Models
-
-
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    inventory_quantity = db.Column(db.Integer, nullable=False)
+    def __repr__(self):
+        return f"{self.name} {self.description} {self.price} {self.inventory_quantity}"
 
 # Schemas
-
-
+class ProductSchema(ma.Schema):
+    id = fields.Integer(primary_key=True)
+    name = fields.String(required=True)
+    description = fields.String(required=True)
+    price = fields.Float(required=True)
+    inventory_quantity = fields.String(required=True)
+    class Meta:
+        fields = ("id", "name", "description", "price", "inventory_quantity")
+    @post_load
+    def create_load(self, data, **kwargs):
+        return Product(**data)
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
 
 # Resources
+class ProductsResource(Resource):
+    def get(self):
+        all_products = Products.query.all()
+        return products_schema.dump(all_products)
+
+    def post(self):
+        print(request)
+        form_data = request.get_json()
+        try:
+            new_product = product_schema.load(form_data)
+            db.session.add(new_product)
+            db.session.commit()
+            return product_schema.dump(new_product), 201
+        except ValidationError as err:
+            return err.messages, 400
+        
+class ProductResource(Resource):
+    def get(self, product_id):
+        product_from_database = Products.query.get_or_404(product_id)
+        return product_schema.dump(product_from_database)
+    
+    def delete(self, product_id):
+        product_from_database = Products.query.get_or_404(product_id)
+        db.session.delete(product_from_database)
+        db.session.commit()
+        return '', 204
+    
+    def put(self, product_id):
+        product_from_database = Products.query.get_or_404(product_id)
+        if "name" in request.json: 
+            product_from_database.name = request.json["name"]
+        if "description" in request.json: 
+            product_from_database.description = request.json["description"]
+        if "price" in request.json: 
+            product_from_database.price = request.json["price"]
+        if "inventory_quantity" in request.json:
+            product_from_database.inventory_quantity = request.json["inventory_quantity"]
+        db.session.commit()
+        return product_schema.dump(product_from_database)
 
 
 
 # Routes
+api.add_resource(ProductsResource, '/api/products')
+api.add_resource(ProductResource, '/api/products/<int:product_id>')
